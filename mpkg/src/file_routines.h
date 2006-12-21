75971bc46392b3b03589501dbc9a06709d618bc3
i27249 <i27249>
2006-12-21 07:19:15 +0000

    Some fixes... I forgotten :-)

--- mpkg/src/file_routines.h
+++ mpkg/src/file_routines.h
@@ -1,6 +1,6 @@
 /******************************************************
 * File operations
-* $Id: file_routines.h,v 1.1 2006/12/20 20:00:38 i27249 Exp $
+* $Id: file_routines.h,v 1.2 2006/12/21 07:19:15 i27249 Exp $
 * ****************************************************/
 #ifndef FILE_ROUTINES_H_
 #define FILE_ROUTINES_H_
@@ -8,7 +8,7 @@
 #include "local_package.h"
 #include <unistd.h>
 #include <errno.h>
-
+#include <libtar.h>
 string get_tmp_file();
 void delete_tmp_files();
 bool FileExists(string filename);
