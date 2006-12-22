3852e4bc3740af98aa86a387127eaabbc6901a4c
adiakin <adiakin>
2006-12-22 10:30:15 +0000

    *** empty log message ***

---
+++ mpkg/src/actions.h
@@ -0,0 +1,14 @@
+#ifndef ACTIONS_H_
+#define ACTIONS_H_
+
+#define ACT_INSTALL	0x01
+#define ACT_REMOVE	0x02
+#define ACT_CLEAN	0x04
+#define ACT_SEARCH	0x05
+#define ACT_UPGRADE	0x06
+#define ACT_UPDATE	0x07
+#define ACT_LIST	0x08
+#define ACT_NONE	0x09
+
+#endif
+
