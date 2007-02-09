a28f2b92a7207d58def1585f46bc794ea7372eb3
i27249 <i27249>
2007-02-09 10:48:05 +0000

    0.1 Beta 1 release

--- mpkg/src/installpkg-ng.cpp
+++ mpkg/src/installpkg-ng.cpp
@@ -4,7 +4,7 @@
  *	New generation of installpkg :-)
  *	This tool ONLY can install concrete local file, but in real it can do more :-) 
  *	
- *	$Id: installpkg-ng.cpp,v 1.30 2007/02/09 10:35:51 i27249 Exp $
+ *	$Id: installpkg-ng.cpp,v 1.31 2007/02/09 10:48:05 i27249 Exp $
  */
 
 #include "config.h"
@@ -36,11 +36,9 @@ int install(string fname, mpkgDatabase *db, DependencyTracker *DepTracker, bool 
 int uninstall(string pkg_name, mpkgDatabase *db, DependencyTracker *DepTracker, int do_purge, bool do_upgrade=false);
 int update_repository_data(mpkgDatabase *db, DependencyTracker *DepTracker);
 int list(mpkgDatabase *db, DependencyTracker *DepTracker, vector<string> search);
-int _clean(const char *filename, const struct stat *file_status, int filetype);
 int clean_cache();
 void ShowBanner();
 int convert_directory(string out_dir);
-int _conv_dir(const char *filename, const struct stat *file_status, int filetype);
 int upgrade (string pkgname, mpkgDatabase *db, DependencyTracker *DepTracker);
 int build_package();
 int list_rep();
@@ -51,7 +49,7 @@ int build_package()
 }
 void ShowBanner()
 {
-	char *version="0.1 alpha 4";
+	char *version="0.1 beta 1";
 	char *copyright="\(c) 2006-2007 RPUNet (http://www.rpunet.ru)";
 	printf("MOPSLinux packaging system v.%s\n%s\n--\n", version, copyright);
 }
@@ -346,191 +344,23 @@ void print_usage(FILE* stream, int exit_code)
 int update_repository_data(mpkgDatabase *db, DependencyTracker *DepTracker)
 {
 	return mpkgSys::update_repository_data(db, DepTracker);
-	/*
-	Repository rep;
-	PACKAGE_LIST availablePackages;
-	PACKAGE_LIST tmpPackages;
-	if (REPOSITORY_LIST.empty())
-	{
-		printf("No repositories defined, add at least one to proceed\n");
-		return -1;
-	}
-
-	printf(_("Updating package data from %d repositories...\n"), REPOSITORY_LIST.size());
-	
-	for (unsigned int i=0; i<REPOSITORY_LIST.size(); i++)
-	{
-		//printf("cycle i=%d\n", i);
-		tmpPackages = rep.get_index(REPOSITORY_LIST[i]);
-		if (i+1==REPOSITORY_LIST.size())
-		{
-			printf("Download completed. Processing data, please wait...\n");
-		}
-		if (tmpPackages.IsEmpty())
-		{
-			printf("No packages found at %s: repository error, or connection error\n", REPOSITORY_LIST[i].c_str());
-			break;
-		}
-		else
-		{
-			//printf(_("Repository has %d packages, infiltrating...\n"), tmpPackages.size());
-			for (int s=0; s<tmpPackages.size(); s++)
-			{
-				tmpPackages.get_package(s)->set_status(PKGSTATUS_AVAILABLE);
-				debug("installpkg-ng.cpp: update_repository_data(): set status to PKGSTATUS_AVAILABLE ("+\
-						IntToStr(tmpPackages.get_package(s)->get_status())+")");
-
-				tmpPackages.get_package(s)->get_locations()->get_location(0)->get_server()->set_url(REPOSITORY_LIST[i]);
-				tmpPackages.get_package(s)->get_locations()->get_location(0)->get_server()->set_priority(IntToStr(i+1));
-
-
-				debug("installpkg-ng.cpp: update_repository_data(): set server url to "+\
-						tmpPackages.get_package(s)->get_locations()->get_location(0)->get_server()->get_url());
-
-			}
-			//printf("Running add_list....");
-			availablePackages.add_list(&tmpPackages);
-			//printf("done.\n");
-			//printf(_("After infiltrating, we have got %d NEW packages"), availablePackages.size());
-
-		}
-	}
-
-#ifdef DEBUG
-	for (int i=0; i<availablePackages.size(); i++)
-	{
-		printf("installpkg-ng.cpp (update_repository_data): package %d has %d locations\n", i, availablePackages.get_package(i)->get_locations()->size());
-	}
-	printf("installpkg-ng.cpp: (update_repository_data): Sending %d packages to db->updateRepositoryData()\n", availablePackages.size());
-#endif
-	// Go merging to DB
-	printf(_("Importing new data...\n"));
-	int ret=db->updateRepositoryData(&availablePackages);
-	printf("Import complete.\n");
-	return ret;
-	*/
 }
 
 int install(string fname, mpkgDatabase *db, DependencyTracker *DepTracker, bool do_upgrade)
 {
 	return mpkgSys::install(fname, db, DepTracker, do_upgrade);
-/*
 
-	//printf(_("Querying the database, please wait...\n"));
-	// Step 1. Checking if it is just a name of a package
-	SQLRecord sqlSearch;
-	sqlSearch.addField("package_name", fname);
-	PACKAGE_LIST candidates;
-	PACKAGE tmp_pkg;
-	db->get_packagelist(sqlSearch, &candidates);
-	debug("candidates size = "+IntToStr(candidates.size()));
-	bool alreadyInstalled=false;
-	if (candidates.size()>0)
-	{
-		for (int i=0; i<candidates.size(); i++)
-		{
-			debug("Candidate #"+IntToStr(i)+": "+candidates.get_package(i)->get_name()+" with ID "+IntToStr(candidates.get_package(i)->get_id())+" and status "+ \
-					candidates.get_package(i)->get_vstatus());
-			if (!do_upgrade && candidates.get_package(i)->get_status()==PKGSTATUS_INSTALLED)
-			{
-				alreadyInstalled=true;
-				printf(_("Package is already installed (ver. %s). For upgrade, choose upgrade option\n"), candidates.get_package(i)->get_version().c_str());
-				break;
-			}
-			if (candidates.get_package(i)->get_status() == PKGSTATUS_AVAILABLE || candidates.get_package(i)->get_status() == PKGSTATUS_REMOVED_AVAILABLE)
-			{
-				debug("Status passed to installation");
-				if (tmp_pkg.IsEmpty() || tmp_pkg.get_version()<candidates.get_package(i)->get_version())
-				{
-					debug("tmp stored successfully");
-					tmp_pkg=*candidates.get_package(i);
-				}
-			}
-		}
-		if (alreadyInstalled)
-		{
-			return 0;
-		}
-		DepTracker->merge(&tmp_pkg);
-		return 0;
-	}
-	debug("LOCAL INSTALL ATTEMPT DETECTED");
-	// If reached this point, the package isn't in the database. Install from local file.
-	// Part 0. Check if file exists.
-	if (!CheckFileType(fname))
-	{
-		printf("%s: unsupported package type\n", fname.c_str());
-		return -1;
-	}
-	struct stat st;
-	if (lstat(fname.c_str(), &st) != 0) {
-		if ( errno == ENOENT ) {
-			fprintf(stderr, _("file %s not found\n"), fname.c_str());
-			exit(1);
-		}
-	}
-	
-	if ( !S_ISREG(st.st_mode) && !S_ISLNK(st.st_mode) ) {
-		fprintf(stderr, _("unknown file type\n"));
-		return 1;
-	}
-	
-	// Part 1. Extracts all information from file and fill the package structure, and insert into dep tracker
-	LocalPackage lp(fname);
-	lp.injectFile(); 
-	lp.data.set_status(PKGSTATUS_AVAILABLE);
-	db->emerge_to_db(&lp.data);
-	DepTracker->merge(&lp.data);
-	return 0;
-*/	
 }
 
 
 int uninstall(string pkg_name, mpkgDatabase *db, DependencyTracker *DepTracker, int do_purge, bool do_upgrade)
 {
 	return mpkgSys::uninstall(pkg_name, db, DepTracker, do_purge, do_upgrade);
-	/*
-	if (do_purge==0) printf(_("You are going to uninstall package %s\n"), pkg_name.c_str());
-	if (do_purge==1) printf(_("You are going to purge package %s\n"), pkg_name.c_str());
-	
-	PACKAGE package=db->get_installed_package(pkg_name);
-	if (package.IsEmpty())
-	{
-		debug("Not installed package");
-		int id;
-		if (do_purge==1) id=db->get_purge(pkg_name);
-		else id=0;
-		if (id==0)
-		{
-			if (do_purge) printf(_("Package %s is already purged\n"), pkg_name.c_str());
-			else printf(_("Package %s is not installed\n"), pkg_name.c_str());
-			return 0;
-		}
-		if (id<0)
-		{
-			printf("Internal error while calling get_purge(): error code = %d\n", id);
-			return id;
-		}
-		printf("setting status to purge for ID %d\n", id);
-		db->set_status(id, PKGSTATUS_PURGE);
-		return 0;
-	}
-	DepTracker->unmerge(&package, do_purge, do_upgrade);
-	return 0;
-	*/
 }
 
 int upgrade (string pkgname, mpkgDatabase *db, DependencyTracker *DepTracker)
 {
 	return upgrade (pkgname, db, DepTracker);
-	/*
-	uninstall(pkgname, db, DepTracker, 0, true);
-	install(pkgname, db, DepTracker, true);
-	DepTracker->normalize();
-	DepTracker->commitToDb();
-	db->commit_actions();
-	return 0;
-	*/
 }
 
 int list_rep()
@@ -575,23 +405,6 @@ int list(mpkgDatabase *db, DependencyTracker *DepTracker, vector<string> search)
 	return 0;
 }
 
-int _clean(const char *filename, const struct stat *file_status, int filetype)
-{
-	/*
-	switch(filetype)
-	{
-		case FTW_F:
-			unlink(filename);
-			break;
-		case FTW_D:
-			rmdir(filename);
-			break;
-		default:
-			unlink(filename);
-	}
-//	printf("."); //Optional =)*/
-	return 0;
-}
 
 int clean_cache()
 {
@@ -603,30 +416,11 @@ int clean_cache()
 	return 0;
 }
 
-int _conv_dir(const char *filename, const struct stat *file_status, int filetype)
-{
-/*	string _package=filename;
-       	string ext;
-	for (unsigned int i=_package.length()-4;i<_package.length();i++)
-	{
-		ext+=_package[i];
-	}
-
-	if (filetype==FTW_F && ext==".tgz")
-	{
-		convert_package(_package, output_dir);
-	}
-	return 0;
-*/
-return 0;
-}
 
 
 int convert_directory(string out_dir)
 {
-//	ftw("./", _conv_dir, 100);
 	return mpkgSys::convert_directory(out_dir);
-	//return 0;
 }
 
 int check_action(char* act)
